0d13e9023819d6f0c14dd8920e6629ce79391ff0
jdchismar <jdchismar@yahoo.com>
2020-03-12 18:55:13 -0700

    Second checkin

---
+++ Manager/ItemManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccess.Models;
+
+namespace Manager
+{
+    public class ItemManager
+    {
+        public List<Item> GetItems()
+        {
+            return new List<Item>();
+        }
+    }
+}
--- MediKeeperAPI/Startup.cs
+++ MediKeeperAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace MediKeeperAPI
 {
@@ -26,6 +27,10 @@ namespace MediKeeperAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,16 @@ namespace MediKeeperAPI
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
